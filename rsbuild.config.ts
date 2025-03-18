6109d079928f6db3303505ffdc16001231d02741
chenhaoli <chenhaoli@bytedance.com>
2025-03-19 04:53:00 +0800

    fix: title, description and favicon

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from "@rsbuild/core";
 import { pluginReact } from "@rsbuild/plugin-react";
 
@@ -6,10 +5,15 @@ export default defineConfig({
   plugins: [pluginReact()],
   source: {
     entry: {
-      index: './src/entry.tsx',
+      index: "./src/entry.tsx",
     },
   },
-  
+  html: {
+    title: "Agent TARS",
+    favicon: 'https://github.com/bytedance/UI-TARS-desktop/blob/main/apps/ui-tars/resources/favicon-32x32.png?raw=true',
+    meta: {
+      description:
+        "An open-source GUI agent designed to revolutionize multimodal interaction",
+    },
+  },
 });
-
-  
