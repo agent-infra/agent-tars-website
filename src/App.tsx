afd285118e865c18031389a8b584fab2e4165770
chenhaoli <chenhaoli@bytedance.com>
2025-03-19 11:08:33 +0800

    feat: add password protection

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
 import Home from "./pages/Home";
 import Blog from "./pages/Blog";
 import Showcase from "./pages/Showcase";
+import { PasswordProtection } from "./components/PasswordProtection";
 
 const App: React.FC = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  if (!isAuthenticated) {
+    return (
+      <PasswordProtection 
+        correctPassword="agentic" 
+        onSuccess={() => setIsAuthenticated(true)} 
+      />
+    );
+  }
+
   return (
     <HashRouter>
         <Navbar />
