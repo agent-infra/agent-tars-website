2304eca03f1d375d0ef5ecaad2d17ca761e5ea94
chenhaoli <chenhaoli@bytedance.com>
2025-03-20 11:46:35 +0800

    feat: integrate GA

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -28,6 +28,7 @@ export default defineConfig({
   },
   html: {
     title: "Agent TARS",
+    template: 'public/index.html',
     favicon:
       "https://github.com/bytedance/UI-TARS-desktop/blob/main/apps/ui-tars/resources/favicon-32x32.png?raw=true",
     meta: {
