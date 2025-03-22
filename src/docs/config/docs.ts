a76a595bf918870ccb6a8e78b13235a95fc55c24
chenhaoli <chenhaoli@bytedance.com>
2025-03-23 00:19:30 +0800

    feat: init Trouble Shooting section

--- src/docs/config/docs.ts
+++ src/docs/config/docs.ts
@@ -12,7 +12,7 @@ export interface DocItem {
 
 export const availableDocs: DocItem[] = [
   {
-    id: "doc/quick-start",
+    id: "quick-start",
     title: "Quick Start",
     // path: "quick-start",
     // using local file for better performance
@@ -21,7 +21,15 @@ export const availableDocs: DocItem[] = [
     // "https://raw.githubusercontent.com/bytedance/UI-TARS-desktop/refs/heads/main/apps/agent-tars/docs/quick-start.md",
     category: "Getting Started",
     publishDate: "2025-03-18",
-    author: "Agent TARS Team",
+    // author: "Agent TARS Team",
+  },
+  {
+    id: "trouble-shooting",
+    title: "Trouble Shooting",
+    localPath: "trouble-shooting",
+    category: "Getting Started",
+    publishDate: "2025-03-22",
+    // author: "Agent TARS Team",
   },
 ];
 
