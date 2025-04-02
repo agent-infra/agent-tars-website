2aa5d69ef2b1a313edf582d725c5365c7169a18a
chenhaoli <chenhaoli@bytedance.com>
2025-04-02 21:59:56 +0800

    refactor: home component

---
+++ src/render/components/Home/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Footer: React.FC = () => {
+  return (
+    <footer className="py-8">
+      <div className="max-w-7xl mx-auto px-4">
+        <div className="text-center text-gray-600">
+          <p>Licensed under Apache License 2.0</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
